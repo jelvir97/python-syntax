dbe237cc88fe672e2d2487ddc80cdcc79c85f49b
Juan Elvir <jelvir97@gmail.com>
2023-05-09 14:27:13 -0600

    added docstring for print_upper_words function

--- words.py
+++ words.py
@@ -1,6 +1,12 @@
 word_list = ['apple','banana','grapefruit','orange','plum']
 
 def print_upper_words(words, *starting_let):
+    """ Accepts list and starting character
+        -sorts through list, ignoring case, finding elements that start 
+        with given letters
+        -prints out selected words in UPPERCASE
+    """
+
     for word in words:
         for letter in starting_let:
             if word.casefold().startswith(letter):
